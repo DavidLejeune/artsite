bc3101252b95ec011e9d4bebffd44e5dcdfe4ff3
DavidLejeune <Lejeune.David@outlook.com>
2021-04-27 16:35:57 +0100

    before leap

--- artworks/urls.py
+++ artworks/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from . import views
-#from .views import PaintingDetailView, PaintingView, HomeView, AddPaintingView, UpdatePaintingView, DeletePaintingView, AddCategoryView, CategoryView
+from .views import ArtworkView, HomeView #, ArtworkDetailView, AddArtworkView, UpdateArtworkView, DeleteArtworkView, AddCategoryView, CategoryView
 urlpatterns = [
-    path('', views.home , name="home"),
-    #path('', HomeView.as_view(), name="home"),
-    #path('painting/<int:pk>', PaintingDetailView.as_view(), name="painting-detail"),
-    #path('paintings/', PaintingView.as_view(), name="paintings"),
-    #path('add_painting/' , AddPaintingView.as_view(), name="add-painting"),
-    #path('painting/edit/<int:pk>' , UpdatePaintingView.as_view(), name="update-painting"),
-    #path('painting/<int:pk>/delete' , DeletePaintingView.as_view(), name="delete-painting"),
+    #path('', views.home , name="home"),
+    path('', HomeView.as_view(), name="home"),
+    #path('artwork/<int:pk>', ArtworkDetailView.as_view(), name="artwork-detail"),
+    path('artworks/', ArtworkView.as_view(), name="artworks"),
+    #path('add_artwork/' , AddArtworkView.as_view(), name="add-artwork"),
+    #path('artwork/edit/<int:pk>' , UpdateArtworkView.as_view(), name="update-artwork"),
+    #path('artwork/<int:pk>/delete' , DeleteArtworkView.as_view(), name="delete-artwork"),
     #path('add_category/' , AddCategoryView.as_view(), name="add-category"),
     #path('category/<str:cats>/' , CategoryView, name="category"),
 
--- artworks/views.py
+++ artworks/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-#from .models import Painting, Category
+from .models import Artwork #, Category
 #from .forms import PaintingForm, EditPaintingForm
 from django.urls import reverse_lazy
 
@@ -8,6 +8,13 @@ from django.urls import reverse_lazy
 def home(request):
     return render(request, 'home.html' , {})
 
-#class HomeView(ListView):
-#    model = Painting
-#    template_name = 'home.html'
+class HomeView(ListView):
+    model = Artwork
+    template_name = 'home.html'
+
+
+class ArtworkView(ListView):
+    model = Artwork
+    template_name = 'artworks.html'
+    #ordering = ['-id']
+    ordering = ['-artwork_date']
