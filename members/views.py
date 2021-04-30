e32026aa111ef37d0692e915a7bd18be80136a5a
DavidLejeune <Lejeune.David@outlook.com>
2021-04-30 02:23:12 +0100

    url for success

--- members/views.py
+++ members/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.urls import reverse_lazy
 from .forms import SignUpForm, EditProfileForm
+from django.contrib.auth.views import PasswordChangeView
 
 # Create your views here.
 
@@ -20,3 +21,8 @@ class UserEditView(generic.UpdateView):
 
     def get_object(self):
         return self.request.user
+
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy('home')
