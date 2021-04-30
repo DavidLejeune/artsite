73b78dcec08e6c7a8d5ad232b653a3bde577cc3b
DavidLejeune <Lejeune.David@outlook.com>
2021-04-30 23:38:48 +0100

    upload ok

--- artsite/settings.py
+++ artsite/settings.py
@@ -146,3 +146,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS  = (
+    os.path.join(BASE_DIR, 'static'),
+)
